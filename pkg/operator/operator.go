5cec868a3c7af8959bb984874a2173a3575258fb
Luke <feixiang.li@smartnews.com>
2024-09-12 14:49:15 +0900

    chore: upgrade karpenter to 1.0.1

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -176,6 +176,18 @@ func NewOperator() (context.Context, *Operator) {
 	lo.Must0(mgr.GetFieldIndexer().IndexField(ctx, &corev1.Node{}, "spec.providerID", func(o client.Object) []string {
 		return []string{o.(*corev1.Node).Spec.ProviderID}
 	}), "failed to setup node provider id indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(ctx, &corev1.Event{}, "involvedObject.kind", func(o client.Object) []string {
+		return []string{o.(*corev1.Event).InvolvedObject.Kind}
+	}), "failed to setup event kind indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(ctx, &corev1.Event{}, "involvedObject.name", func(o client.Object) []string {
+		return []string{o.(*corev1.Event).InvolvedObject.Name}
+	}), "failed to setup event name indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(ctx, &corev1.Event{}, "namespace", func(o client.Object) []string {
+		return []string{o.(*corev1.Event).Namespace}
+	}), "failed to setup event namespace indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(ctx, &corev1.Event{}, "reason", func(o client.Object) []string {
+		return []string{o.(*corev1.Event).Reason}
+	}), "failed to setup event reason indexer")
 	lo.Must0(mgr.GetFieldIndexer().IndexField(ctx, &v1.NodeClaim{}, "status.providerID", func(o client.Object) []string {
 		return []string{o.(*v1.NodeClaim).Status.ProviderID}
 	}), "failed to setup nodeclaim provider id indexer")
